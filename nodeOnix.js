3456c44ea983474bb85798d00382358ebb0bed93
DimaGrimach <chiki.briki2@gmail.com>
2022-10-18 00:49:44 +0300

    Get rates ctypro for Node.js Onix Internship 2022

---
+++ nodeOnix.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const https = require('https')
+const hostname = '127.0.0.1'
+const fs = require('fs')
+const path = require('path')
+const port = 3000
+const reqUrl = 'api.coincap.io'
+
+
+const server = http.createServer((req, res) => {
+    let currency = new URLSearchParams(req.url).get('/rates?currency');
+    if (!currency) {
+        res.writeHead(404);
+        res.end('Error- absent name currency');
+        return;
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    let result = '';
+    https.get('https://api.coincap.io/v2/rates/'+currency, (resp) => {
+        let data = '';
+        resp.on('data', chunk => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            let obj = JSON.parse(data);
+            result = obj.data.rateUsd;
+        })
+    });
+
+    setTimeout(() => res.end("usd :" + result), 400);
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
